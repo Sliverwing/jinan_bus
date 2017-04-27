aae42eb09fb25a104a610fc91f785f39323bd8f4
Sliverwing <tlv591208510@gmail.com>
2017-04-27 22:46:08 +0800

    config loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,29 @@ var path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, './resources/js/app.js'),
     output: {
-        path: path.resolve(__dirname, 'public/js/'),
+        path: path.resolve(__dirname, 'public/'),
         filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css?$/,
+                loader: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
     }
 }
